204a8eead4706016750a74a87e48206e6d295593
sweety <smehta1289@gmail.com>
2013-12-05 07:04:18 +0530

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ DropdownAppearenceThruAjax::Application.routes.draw do
     end
   end
 
-
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
